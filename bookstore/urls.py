de28393680de0ca9476d0a7f5763d5e580e83d9a
bobbyprs <32490171+bobbyprs@users.noreply.github.com>
2021-05-10 22:56:18 +0530

    Update urls.py

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -21,7 +21,7 @@ from django.views.static import serve
 from django.conf.urls import url
 
 urlpatterns = [
-    path('admin/' , admin.site.urls),
+#     path('admin/' , admin.site.urls),
     path('', include('inventoryApp.urls')),
     url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
